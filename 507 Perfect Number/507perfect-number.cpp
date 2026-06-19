2dc94bc5738f27d19ad4960fa11a01c6b2e66e81
Mihir Agrawal <25bec064@gmail.com>
2026-06-19 12:07:25 +0000

    Code challenge submission for question: 507 Perfect Number

---
+++ 507 Perfect Number/507perfect-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int count = 0;
+        for(int i = 1; i<= num/2; i++){
+            if(num%i == 0){
+                count+=i;
+            }
+        }
+        if(count == num){
+            return 1;
+        }
+        return 0;
+    }
+};
