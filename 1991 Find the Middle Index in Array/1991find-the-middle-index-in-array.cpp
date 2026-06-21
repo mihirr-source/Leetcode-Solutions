fb0e16547a6fc425c630735712ba16fbdb125bac
Mihir Agrawal <25bec064@gmail.com>
2026-06-21 14:15:01 +0000

    Code challenge submission for question: 1991 Find the Middle Index in Array

---
+++ 1991 Find the Middle Index in Array/1991find-the-middle-index-in-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        for(int i = 0; i<nums.size(); i++){
+            if(i == 0 && accumulate(nums.begin() + 1, nums.end(),0) == 0){
+                return 0;
+            }
+            else if(i ==nums.size()-1 && accumulate(nums.rbegin()+1, nums.rend(),0) ==0){
+                return i;
+            }
+            else if( accumulate(nums.begin()+i+1, nums.end(),0) == accumulate(nums.rbegin()+nums.size()-i, nums.rend(),0)){
+
+                return i;
+            }
+        }
+        return -1;
+    }
+};
